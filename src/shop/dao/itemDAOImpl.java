8264790627ab0b42cbd3f91af199e2aea71593d1
402A-1 <402A-1@402A-1-PC>
2016-02-04 17:02:19 +0900

    4

--- src/shop/dao/itemDAOImpl.java
+++ src/shop/dao/itemDAOImpl.java
@@ -16,10 +16,21 @@ public class itemDAOImpl implements ItemDAO {
 		//유효성 검사는 svc에서
 		int num=-1; //성공 여부를 표기. 성공은 1,실패는 10
 		SqlSession session = factory.openSession();
-		session.insert("",item);
+		session.insert("item.insert",item);
 		
+		if (selectId(item.getItemId())==null) {
+			num=10;
+		}else {
+			num=1;
+		}
 		session.close();
-		return 0;
+		return num;
+		
+	}
+
+	public Item selectId(String itemId) {
+		return null;
+		// TODO Auto-generated method stub
 		
 	}
 }
