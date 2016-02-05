08e196099971d5f50794fcb3048a759f5b9d2181
402A-1 <402A-1@402A-1-PC>
2016-02-05 11:19:54 +0900

    02051119

--- src/shop/controller/ItemController.java
+++ src/shop/controller/ItemController.java
@@ -1,8 +1,14 @@
 package shop.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import shop.dto.Item;
 import shop.svc.ItemService;
 
 @Controller
@@ -11,4 +17,11 @@ public class ItemController {
 @Autowired
 ItemService svc;
 	
+
+@RequestMapping("/itemList.do")
+public String ItemList(Model model){
+	ArrayList<Item> list = svc.itemList();
+	model.addAttribute("itemList",list);
+	return "list";
+}
 }
